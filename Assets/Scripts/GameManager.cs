4f97fbe57a5b88e47a60a462d4e4f0a8cc0b11e3
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-04-02 17:52:08 +0900

    캐릭터 코딩 및 점수 갱신 기능 구현

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject rain;
+    public static GameManager I;
+    public Text scoreText;
+    int totalScore;
+
+    private void Awake()
+    {
+        I = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("makeRain", 0, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void makeRain()
+    {
+        Instantiate(rain);
+    }
+
+    public void addScore(int score)
+    {
+        totalScore += score;
+        scoreText.text = totalScore.ToString(); 
+    }
+}
