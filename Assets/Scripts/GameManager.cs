41134c4b5b11e838599975c23f03bfdd4f026a51
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-04-02 18:07:02 +0900

    빗물 받는 르탄이 게임 완성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject rain;
+    public GameObject panel;
     public static GameManager I;
     public Text scoreText;
+    public Text timeText;
     int totalScore;
+    float limit = 5f;
 
     private void Awake()
     {
@@ -17,13 +21,21 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        initGame();
         InvokeRepeating("makeRain", 0, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        limit -= Time.deltaTime;
+        if (limit < 0)
+        {
+            Time.timeScale = 0.0f;
+            panel.SetActive(true);
+            limit = 0.0f;
+        }
+        timeText.text = limit.ToString("N2");
     }
 
     void makeRain()
@@ -35,5 +47,17 @@ public class GameManager : MonoBehaviour
     {
         totalScore += score;
         scoreText.text = totalScore.ToString(); 
+    }
+
+    public void retry()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    void initGame()
+    {
+        Time.timeScale = 1.0f;
+        totalScore = 0;
+        limit = 5f;
     }
 }
